729ae0e793280f9a39ab0b35c0653a15e6c3fbb8
EchoAlice <echoalice0@gmail.com>
2022-05-26 14:10:48 -0400

    Creates a callsAPI() and getsCurrentBlockHeader() function

--- light_client/mvplightclient.py
+++ light_client/mvplightclient.py
@@ -1,5 +1,7 @@
 import requests
 import json
+
+# from containers import BeaconBlockHeader
 # from containers import LightClientUpdate
 # from containers import LightClientStore
 # from helperfunctions import is_finality_update
@@ -8,11 +10,22 @@ import json
 # from helperfunctions import get_safety_threshold
 
 #  MVP Light Client:  Track latest state/block root
+def callsAPI(url):
+  response = requests.get(url)
+  json_object = response.json() 
+  return json_object
+
+def getsCurrentBlockHeader(url):
+  return callsAPI(url)
 
 if __name__ == "__main__":
   # Initialization:
-  # Query API for current block header
-  response = requests.get("https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers")
-  block_header = response.json()
+  block_header_url =  "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers"
+  block_header = getsCurrentBlockHeader(block_header_url)
   beacon_block_header_container = block_header['data'][0]['header']['message']
-  print(beacon_block_header_container)
+  print(beacon_block_header_container) 
+  
+  # Figure out how to instantiate container with API message 
+  # Do the message, or individual data types within the container need to be serialized?
+    
+  # print(BeaconBlockHeader)
