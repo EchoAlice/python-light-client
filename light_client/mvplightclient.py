8a6ef121770cb5121803c44f40a51baca15b479b
EchoAlice <echoalice0@gmail.com>
2022-05-28 13:40:03 -0400

    Creates API call for checkpoint and gets checkpoint root

--- light_client/mvplightclient.py
+++ light_client/mvplightclient.py
@@ -17,10 +17,27 @@ def callsAPI(url):
 
 if __name__ == "__main__":
   # Initialization:
+  #
+  # Place all initialization and sync info into the LightClientStore data class
+  #
+  # Ask node for current sync committee + proof of checkpoint root
+  # Node responds with:
+  #    header- Block's header corresponding to the checkpoint root (What's the checkpoint root?)
+  #    current sync committee- Public Keys and the aggregated pub keyof the current sync committee
+  #    current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+
+  # Gets checkpoint root
+  checkpoint_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/states/head/finality_checkpoints" 
+  checkpoint = callsAPI(checkpoint_url)
+  checkpoint_root = checkpoint['data']['finalized']['root']  
+  print(checkpoint_root) 
+
+  # Place checkpoint root in block_header_url API call 
   block_header_url =  "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers"
   block_header = callsAPI(block_header_url)
+  
   beacon_block_header_container = block_header['data'][0]['header']['message']
-  print(beacon_block_header_container) 
+  # print(beacon_block_header_container)
   
   # Figure out how to instantiate container with API message 
   # Do the message, or individual data types within the container need to be serialized?
