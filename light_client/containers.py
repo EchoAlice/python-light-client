cf141d3bee749a9af5dcfb7807dd18f8c442a963
EchoAlice <echoalice0@gmail.com>
2022-05-30 17:59:32 -0400

    Provides insightful comments as guidelines for development

--- light_client/containers.py
+++ light_client/containers.py
@@ -11,6 +11,7 @@ ValidatorIndex	= uint64
 Gwei =	uint64	
 # How do i turn this data type into something I can use in python?
 # Root =	Bytes32   <--- Should be implemented like this	
+SyncCommittee = [uint64]
 Root = bytes	
 # Hash32	= Bytes32
 # Version	= Bytes4	
@@ -20,6 +21,18 @@ Root = bytes
 # BLSPubkey =	Bytes48
 # BLSSignature = Bytes96
 
+# Constants for merkle proofs:
+#
+# Generalized indices for finalized checkpoint and next sync committee in a BeaconState.
+# A Generalized index is a way of referring to a poisition of an object in a merkle tree,
+# so that the Merkle proof verification algorithm knows what path to check the hashes against
+#
+# FINALIZED_ROOT_INDEX = get_generalized_index(BeaconState, 'finalized_checkpoint', 'root')
+# NEXT_SYNC_COMMITTEE_INDEX = get_generalized_index(BeaconState, 'next_sync_committee')
+
+# If all goes well, we'll update our light client memory with this header
+# The header is the key trusted piece of data we use to verify merkle proofs against.
+# From a beacon block, we can use merkle proofs to verify data about everything.  ie beacon state
 class BeaconBlockHeader(Container):
     slot: Slot
     proposer_index: ValidatorIndex
@@ -27,23 +40,26 @@ class BeaconBlockHeader(Container):
     state_root: Root
     body_root: Root
 
+# This is the data we request to stay synced.  We need an update every 27(ish) hours
 class LightClientUpdate(Container):
   # The beacon block header that is attested to by the sync committee
   attested_header: BeaconBlockHeader
   # Next sync committee corresponding to the active header
+  # The committee branch is a merkle proof that we run against the header
   next_sync_committee: SyncCommittee
   next_sync_committee_branch: Vector[Bytes32, floorlog2(NEXT_SYNC_COMMITTEE_INDEX)]
   # The finalized beacon block header attested to by Merkle branch
+  # The header branch is a merkle proof that we run agains the shard block root
   finalized_header: BeaconBlockHeader
   finality_branch: Vector[Bytes32, floorlog2(FINALIZED_ROOT_INDEX)]
   # Sync committee aggregate signature
   sync_aggregate: SyncAggregate
-  # Fork version for the aggregate signature
+  # Fork version for the aggregate signature.  This lets us make sure the votes are for the fork we think we're on
   fork_version: Version
 
 @dataclass
 class LightClientStore(object):
-  # Beacon block header that is finalized
+  # Beacon block header that is finalized (not expected to revert)
   finalized_header: BeaconBlockHeader
   # Sync committees corresponding to the header
   current_sync_committee: SyncCommittee
--- light_client/mvplightclient.py
+++ light_client/mvplightclient.py
@@ -15,31 +15,76 @@ def callsAPI(url):
   json_object = response.json() 
   return json_object
 
+def getBeaconBlockHeader():
+  print("Get information needed for a block header")
+
 if __name__ == "__main__":
+  # If I want to make this light client for the cross chain bridge, the code will need to be written in Solidity.
+  # Light client for cross chain bridge is ran as a smart contract on the destination chain!
+  # 
   # Initialization:
   #
-  # Place all initialization and sync info into the LightClientStore data class
+  # Place all information relating to initialization and syncronization in the LightClientStore data class.
+  # This information needs to follow SSZ container specifications
   #
-  # Ask node for current sync committee + proof of checkpoint root
-  # Node responds with:
-  #    header- Block's header corresponding to the checkpoint root (What's the checkpoint root?)
-  #    current sync committee- Public Keys and the aggregated pub keyof the current sync committee
-  #    current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+  #   Get block header at slot N in period X = N // 16384
+  #   Ask node for current sync committee + proof of checkpoint root
+  #   
+  #   Node responds with:
+  #     header- Block's header corresponding to the checkpoint root (What's the checkpoint root?)
+  #     current sync committee- Public Keys and the aggregated pub keyof the current sync committee
+  #     current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+  #
+  #   A word on Headers:
+  #     The light client stores a header so it can ask for merkle breanches to 
+  #     authenticate transactions and state against the header (I want to learn about how this happens)
+  #
+  #   A word on Sync committees: 
+  #     The purpose of the sync committee is to allow light clients to keep track
+  #     of the chain of beacon block headers. 
+  #     
+  #     Sync committees are (i) updated infrequently, and (ii) saved directly in the beacon state, 
+  #     allowing light clients to verify the sync committee with a Merkle branch from a 
+  #     block header that they already know about, and use the public keys 
+  #     in the sync committee to directly authenticate signatures of more recent blocks.
 
-  # Gets checkpoint root
-  checkpoint_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/states/head/finality_checkpoints" 
+
+  # A first milestone for a light client implementation is to simply HAVE A LIGHT CLIENT THAT SIMPLY TRACKS THE LATEST STATE/BLOCK ROOT.
+
+  # Summary of what light client needs to do 
+  # block_header = getBeaconBlockHeader()
+  # verifyBlockHeader(block_header, sync_committee, sync_committee_branch)
+  # LightClientStore = syncLightClientTo(block_header)
+  # if update_light_client() == true:
+  #   updateLightClient(LightClientStore)
+
+
+  # Gets finalized checkpoint root
+  checkpoint_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/states/finalized/finality_checkpoints"
   checkpoint = callsAPI(checkpoint_url)
+  print(checkpoint['data']['finalized']) 
   checkpoint_root = checkpoint['data']['finalized']['root']  
-  print(checkpoint_root) 
+  # Returns finality checkpoints for finalized state id
+  print(checkpoint_root)
+  print("\n") 
 
   # Place checkpoint root in block_header_url API call 
-  block_header_url =  "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers"
-  block_header = callsAPI(block_header_url)
-  
-  beacon_block_header_container = block_header['data'][0]['header']['message']
-  # print(beacon_block_header_container)
+  # Returns specified block header
+  specified_block_header_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers/0x3cf0393a193a9a95568700946fba64cdca2fdadbb571f40f230e93e5c457032f" 
+  specified_block_header = callsAPI(specified_block_header_url)
+  print("Specified: ") 
+  print(specified_block_header['data']['header']['message'])
+  print("\n")
+  print("Finalized: ")
+
+  # I believe the finalized block header is the header I need to use to initialize the LC 
+  # Returns finalized block header
+  # The header is the key trusted piece of data we use to verify merkle proofs against.
+  # From a beacon block, we can use merkle proofs to verify data about everything
+  finalized_block_header_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers/finalized"
+  finalized_block_header = callsAPI(finalized_block_header_url)
+  print(finalized_block_header['data']['header']['message']) 
   
   # Figure out how to instantiate container with API message 
   # Do the message, or individual data types within the container need to be serialized?
     
-  # print(BeaconBlockHeader)
