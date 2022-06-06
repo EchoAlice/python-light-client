eb5e7b085a0c876ab55f168a4262da5a07224b45
Bebbett <broycroft99@gmail.com>
2022-06-06 16:48:03 -0400

    Deletes unneccessary code

--- light_client/mvplightclient.py
+++ light_client/mvplightclient.py
@@ -24,10 +24,7 @@ if __name__ == "__main__":
   #   Get block header at slot N in period X = N // 16384
   #   Ask node for current sync committee + proof of checkpoint root
   #   
-  #   Node responds with:
-  #     header- Block's header corresponding to the checkpoint root (What's the checkpoint root?)
-  #     current sync committee- Public Keys and the aggregated pub keyof the current sync committee
-  #     current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+  #   Node responds with a snapshot
   #
   #   A word on Headers:
   #     The light client stores a header so it can ask for merkle breanches to 
@@ -44,9 +41,10 @@ if __name__ == "__main__":
 
   # A first milestone for a light client implementation is to HAVE A LIGHT CLIENT THAT SIMPLY TRACKS THE LATEST STATE/BLOCK ROOT.
 
-  # Summary of what light client needs to do 
-  # block_header = getBeaconBlockHeader()
-  # verifyBlockHeader(block_header, sync_committee, sync_committee_branch)
+  # Summary of what light client needs to do: 
+  # checkpoint_root = request(finality_checkpoint)
+  # snapshot = request(snapshot(checkpoint_root))
+  # verifyCurrentSyncCommitteeBranch(snapshot)
   # LightClientStore = syncLightClientTo(block_header)
   # if update_light_client() == true:
   #   updateLightClient(LightClientStore)
@@ -54,33 +52,19 @@ if __name__ == "__main__":
   # Step 1:  Gather most recent finality checkpoint (pretty much a weak subjectivity checkpoint)
   checkpoint_url = "https://api.allorigins.win/raw?url=https://lodestar-mainnet.chainsafe.io/eth/v1/beacon/states/head/finality_checkpoints" 
   checkpoint = callsAPI(checkpoint_url)
-  checkpoint_root = checkpoint['data']['current_justified']['root']  
-  checkpoint_epoch = checkpoint['data']['finalized']['epoch'] 
-  print(checkpoint['data']['finalized'])
+  checkpoint_root = checkpoint['data']['finalized']['root']  
+  print(checkpoint_root)
 
-  # Call lightclient/snapshot with most recent checkpoint root to bootstrap to a period
-  # How do you bootstrap to a period? 
+  # Call lightclient/snapshot with most recent checkpoint root to bootstrap to a period (How do you bootstrap to a period)
+  # A snapshot contains:
+  #     header- Block's header corresponding to the checkpoint root
+  #     current sync committee- Public Keys and the aggregated pub keyof the current sync committee
+  #     current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+  
   snapshot_url = "https://lodestar-mainnet.chainsafe.io/eth/v1/lightclient/snapshot/0x354946e0e14432c9671317d826c10cc3b91d0690c4e8099dce1749f950cd63b3" 
   snapshot = callsAPI(snapshot_url) 
+  print(snapshot) 
   list_of_keys = snapshot['data']['current_sync_committee']['pubkeys']
-  print(list_of_keys)
-  
-  
-  # Gets sync committee associated with the given checkpoint root
-  sync_committee_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/states/head/sync_committees"
-  sync_committee = callsAPI(sync_committee_url)
-  # Returns all validator indices in the current sync committee
-  # print(sync_committee)
-
-  # I believe the finalized block header is the header I need to use to initialize the LC 
-  # Returns finalized block header
-  # The header is the key trusted piece of data we use to verify merkle proofs against.
-  # From a beacon block, we can use merkle proofs to verify data about everything
-  finalized_block_header_url = "https://api.allorigins.win/raw?url=http://testing.mainnet.beacon-api.nimbus.team/eth/v1/beacon/headers/finalized"
-  finalized_block_header = callsAPI(finalized_block_header_url)
-  block_header_message =  finalized_block_header['data']['header']['message']
-  # print("\n")
-  # print(finalized_block_header['data']['header']['message']) 
   
   # Figure out how to instantiate container with API message 
   # Do the message, or individual data types within the container need to be serialized?
