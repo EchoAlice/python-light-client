9ba149bd8feab12594b0247d6158a1c2f4c71d3e
Bebbett <broycroft99@gmail.com>
2022-06-11 15:11:57 -0400

    Creates a Checkpoint container class

--- light_client/containers.py
+++ light_client/containers.py
@@ -43,38 +43,42 @@ class BeaconBlockHeader(Container):
   state_root: Root
   body_root: Root
 
+class Checkpoint(Container):
+    epoch: Epoch
+    root: Root
+
 class SyncCommittee(Container):
   pubkeys: Vector[BLSPubkey, SYNC_COMMITTEE_SIZE]
   aggregate_pubkey: BLSPubkey
 
-# This is the data we request to stay synced.  We need an update every 27(ish) hours
-class LightClientUpdate(Container):
-  # The beacon block header that is attested to by the sync committee
-  attested_header: BeaconBlockHeader
-  # Next sync committee corresponding to the active header
-  # The committee branch is a merkle proof that we run against the header
-  next_sync_committee: SyncCommittee
-  next_sync_committee_branch: Vector[Bytes32, floorlog2(NEXT_SYNC_COMMITTEE_INDEX)]
-  # The finalized beacon block header attested to by Merkle branch
-  # The header branch is a merkle proof that we run agains the shard block root
-  finalized_header: BeaconBlockHeader
-  finality_branch: Vector[Bytes32, floorlog2(FINALIZED_ROOT_INDEX)]
-  # Sync committee aggregate signature
-  sync_aggregate: SyncAggregate
-  # Fork version for the aggregate signature.  This lets us make sure the votes are for the fork we think we're on
-  fork_version: Version
+# # This is the data we request to stay synced.  We need an update every 27(ish) hours
+# class LightClientUpdate(Container):
+#   # The beacon block header that is attested to by the sync committee
+#   attested_header: BeaconBlockHeader
+#   # Next sync committee corresponding to the active header
+#   # The committee branch is a merkle proof that we run against the header
+#   next_sync_committee: SyncCommittee
+#   next_sync_committee_branch: Vector[Bytes32, floorlog2(NEXT_SYNC_COMMITTEE_INDEX)]
+#   # The finalized beacon block header attested to by Merkle branch
+#   # The header branch is a merkle proof that we run agains the shard block root
+#   finalized_header: BeaconBlockHeader
+#   finality_branch: Vector[Bytes32, floorlog2(FINALIZED_ROOT_INDEX)]
+#   # Sync committee aggregate signature
+#   sync_aggregate: SyncAggregate
+#   # Fork version for the aggregate signature.  This lets us make sure the votes are for the fork we think we're on
+#   fork_version: Version
 
-@dataclass
-class LightClientStore(object):
-  # Beacon block header that is finalized (not expected to revert)
-  finalized_header: BeaconBlockHeader
-  # Sync committees corresponding to the header
-  current_sync_committee: SyncCommittee
-  next_sync_committee: SyncCommittee
-  # Best available header to switch finalized head to if we see nothing else
-  best_valid_update: Optional[LightClientUpdate]
-  # Most recent available reasonably-safe header
-  optimistic_header: BeaconBlockHeader
-  # Max number of active participants in a sync committee (used to calculate safety threshold)
-  previous_max_active_participants: uint64
-  current_max_active_participants: uint64
+# @dataclass
+# class LightClientStore(object):
+#   # Beacon block header that is finalized (not expected to revert)
+#   finalized_header: BeaconBlockHeader
+#   # Sync committees corresponding to the header
+#   current_sync_committee: SyncCommittee
+#   next_sync_committee: SyncCommittee
+#   # Best available header to switch finalized head to if we see nothing else
+#   best_valid_update: Optional[LightClientUpdate]
+#   # Most recent available reasonably-safe header
+#   optimistic_header: BeaconBlockHeader
+#   # Max number of active participants in a sync committee (used to calculate safety threshold)
+#   previous_max_active_participants: uint64
+#   current_max_active_participants: uint64
