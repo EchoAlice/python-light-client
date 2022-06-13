da58615379464410391e0d4dc7a96c5a35d9212f
Bebbett <broycroft99@gmail.com>
2022-06-12 23:01:24 -0400

    Creates BeaconBlockHeader object

--- light_client/mvplightclient.py
+++ light_client/mvplightclient.py
@@ -1,8 +1,9 @@
 import requests
+from eth2spec.utils.hash_function import hash
 from remerkleable.basic import bit, uint, uint8, uint64
 from remerkleable.complex import List
 from remerkleable.core import View
-from containers import SyncCommittee
+from containers import BeaconBlockHeader, SyncCommittee
 from merkletreelogic import checkMerkleProof
 
 # A first milestone for a light client implementation is to HAVE A LIGHT CLIENT THAT SIMPLY TRACKS THE LATEST STATE/BLOCK ROOT.
@@ -65,8 +66,18 @@ if __name__ == "__main__":
   checkpoint = callsAPI(checkpoint_url)
   finalized_checkpoint_root = checkpoint['data']['finalized']['root']  
 
+  # BEACON BLOCK HEADER- 
+  beacon_block_header_url = "https://api.allorigins.win/raw?url=https://lodestar-mainnet.chainsafe.io/eth/v1/beacon/headers/0x6b1a3fd7565d41ae1860d976e836842c71f9aee7aeada03ca1e4abf1dd789aef"
+  beacon_block_header = callsAPI(beacon_block_header_url)
+  beacon_info = beacon_block_header['data']['header']['message']
+  beacon_slot = beacon_info['slot']
+  beacon_proposer_index = beacon_info['proposer_index']
+  beacon_parent_root = beacon_info['parent_root']
+  beacon_state_root = beacon_info['state_root']
+  beacon_body_root = beacon_info['body_root']
+
   # SNAPSHOT-
-  snapshot_url = "https://lodestar-mainnet.chainsafe.io/eth/v1/lightclient/snapshot/0x354946e0e14432c9671317d826c10cc3b91d0690c4e8099dce1749f950cd63b3" 
+  snapshot_url = "https://lodestar-mainnet.chainsafe.io/eth/v1/lightclient/snapshot/0x6b1a3fd7565d41ae1860d976e836842c71f9aee7aeada03ca1e4abf1dd789aef" 
   snapshot = callsAPI(snapshot_url)
   list_of_keys = snapshot['data']['current_sync_committee']['pubkeys']
   hex_aggregate_pubkey = snapshot['data']['current_sync_committee']['aggregate_pubkey']
@@ -85,6 +96,19 @@ if __name__ == "__main__":
   #   CHECKPOINT-
   finalized_checkpoint_root =  parseHexToByte(finalized_checkpoint_root)
 
+  #   BEACON BLOCK HEADER-
+  print(beacon_info)
+  print("\n")
+  
+  # I MAY HAVE TO TURN THE SLOT AND INDEX INTO UINT64s 
+  # beacon_slot = uint64(beacon_slot) 
+  # print(type(beacon_slot)) 
+  # beacon_proposer_index = uint64(beacon_proposer_index)
+  beacon_parent_root = parseHexToByte(beacon_parent_root) 
+  beacon_state_root = parseHexToByte(beacon_state_root) 
+  beacon_body_root = parseHexToByte(beacon_body_root) 
+
+
   #   SYNC COMMITTEE- 
   #       List of Keys 
   for i in range(len(list_of_keys)):
@@ -100,14 +124,23 @@ if __name__ == "__main__":
   # ------------------
   # CREATE SSZ OBJECT
   # ------------------
-  
+
+  # ERROR HERE.   I NEED TO MAKE THE BEACON SLOT AN UINT64!!!  Then I'll be able to check the proof against the correct root
+  # BlockBodyHeader
+  current_header = BeaconBlockHeader(
+    slot = beacon_slot,
+    proposer_index = beacon_proposer_index,
+    parent_root = beacon_parent_root,
+    state_root = beacon_state_root,
+    body_root = beacon_body_root,
+  )  
+
   # SyncCommittee
   current_sync_committee = SyncCommittee(
     pubkeys = list_of_keys,
     aggregate_pubkey = current_aggregate_pubkey
   )
- 
-  
+
 
   # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
   # =================================================
@@ -115,7 +148,6 @@ if __name__ == "__main__":
   # =================================================
   # /////////////////////////////////////////////////
 
-  #  Current_sync_committee_branch contains 5 nodes.  
   # 
   #  Merkleize the sync committee object, then hash it against the merkle branch
   #  If the output matches the checkpoint root... Yaaaay 
@@ -123,9 +155,9 @@ if __name__ == "__main__":
   # -----------------------------------
   # MERKLEIZE THE SYNC COMMITTEE OBJECT
   # -----------------------------------
+  beacon_block_header_root = View.hash_tree_root(beacon_block_header) 
   sync_committee_root = View.hash_tree_root(current_sync_committee) 
-  print(sync_committee_root)
-
+  
   # This was too easy.  Do this myself!!!
   # merkleizeSyncCommittee(current_sync_committee)
   
@@ -133,7 +165,12 @@ if __name__ == "__main__":
   # -----------------------------------
   # HASH NODE AGAINST THE MERKLE BRANCH
   # -----------------------------------
-  
-  checkMerkleProof(finalized_checkpoint_root, current_sync_committee, current_sync_committee_branch)
-  
-  
+
+  # Check proof function works, BUT the values still aren't matching up
+  # Am I comparing this hashed value to the wrong hash_tree_root?
+  # The answer isn't just to hash tree root the checkpoint container
+
+  # 2^5 == 32 <--- number of nodes in the 
+  # Compare hashed answer to the BEACON BLOCK HEADER that the sync committee is a part of!
+
+  checkMerkleProof(beacon_block_header_root, sync_committee_root, current_sync_committee_branch)
