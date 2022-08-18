8235c8919659136fa0e8ac7fca1d460ae1c9fc40
Bebbett <broycroft99@gmail.com>
2022-08-18 06:27:59 -1100

    Clean up api.py

--- src/api.py
+++ src/api.py
@@ -12,35 +12,21 @@ from helper import(call_api,
                    updates_for_period,
 )
 
+# Should the return objects be written like this? 
+#   light_client_update = LightClientUpdate(...)
+#   return light_client_update
 def initialize_block_header(header_message):
-  block_header = BeaconBlockHeader (
+  return BeaconBlockHeader (
     slot =  int(header_message['slot']),
     proposer_index = int(header_message['proposer_index']),
     parent_root = parse_hex_to_byte(header_message['parent_root']),
     state_root = parse_hex_to_byte(header_message['state_root']),
     body_root = parse_hex_to_byte(header_message['body_root'])
   )
-  return block_header
 
-# For some reason, I can't parse the list of keys or the agg pub key within the sync committee object
-def initialize_sync_committee(committee_message):
-  list_of_keys = committee_message['pubkeys']
-  aggregate_pubkey = committee_message['aggregate_pubkey']
-  parse_list(list_of_keys)
-  aggregate_pubkey = parse_hex_to_byte(aggregate_pubkey)
-  
-  sync_committee = SyncCommittee(
-    pubkeys = list_of_keys,
-    aggregate_pubkey = aggregate_pubkey
-  )
-  return sync_committee
-
-# See if I can compress this... I run into errors when i try  :/  
 def initialize_sync_aggregate(aggregate_message):
-  sync_committee_hex = aggregate_message['sync_committee_bits']
-  sync_committee_signature = aggregate_message['sync_committee_signature']
-  sync_committee_bits = parse_hex_to_bit(sync_committee_hex) 
-  sync_committee_signature = parse_hex_to_byte(sync_committee_signature)
+  sync_committee_bits = parse_hex_to_bit(aggregate_message['sync_committee_bits'])
+  sync_committee_signature = parse_hex_to_byte(aggregate_message['sync_committee_signature'])
 
   sync_aggregate = SyncAggregate(
     sync_committee_bits = sync_committee_bits, 
@@ -48,6 +34,15 @@ def initialize_sync_aggregate(aggregate_message):
   )
   return sync_aggregate
 
+def initialize_sync_committee(committee_message):
+  list_of_keys = parse_list(committee_message['pubkeys'])
+  aggregate_pubkey = parse_hex_to_byte(committee_message['aggregate_pubkey'])
+  
+  sync_committee = SyncCommittee(
+    pubkeys = list_of_keys,
+    aggregate_pubkey = aggregate_pubkey
+  )
+  return sync_committee
 
 
 #  =================================
@@ -64,8 +59,7 @@ bootstrap = call_api(bootstrap_url).json()
 finalized_checkpoint_root = checkpoint['data']['finalized']['root']  
 bootstrap_header_message = bootstrap['data']['header']
 bootstrap_committee_message = bootstrap['data']['current_sync_committee']
-bootstrap_sync_committee_branch = bootstrap['data']['current_sync_committee_branch']
-parse_list(bootstrap_sync_committee_branch) 
+bootstrap_sync_committee_branch = parse_list(bootstrap['data']['current_sync_committee_branch'])
 
 bootstrap_block_header = initialize_block_header(bootstrap_header_message)
 bootstrap_sync_committee = initialize_sync_committee(bootstrap_committee_message)
@@ -76,30 +70,22 @@ bootstrap_object = LightClientBootstrap(
   current_sync_committee_branch= bootstrap_sync_committee_branch
 )
 
-
-
 #  ================
 #  UPDATE FUNCTIONS
 #  ================ 
 
 def instantiate_sync_period_data(sync_period):
   sync_period_update = updates_for_period(sync_period).json()
-
+  
   attested_block_header = initialize_block_header(sync_period_update['data'][0]['attested_header']) 
   next_sync_committee = initialize_sync_committee(sync_period_update['data'][0]['next_sync_committee'])
+  next_sync_committee_branch = parse_list(sync_period_update['data'][0]['next_sync_committee_branch'])
   finalized_block_header = initialize_block_header(sync_period_update['data'][0]['finalized_header']) 
+  finality_branch = parse_list(sync_period_update['data'][0]['finality_branch'])
   sync_aggregate = initialize_sync_aggregate(sync_period_update['data'][0]['sync_aggregate'])
- 
-  next_sync_committee_branch = sync_period_update['data'][0]['next_sync_committee_branch']
-  parse_list(next_sync_committee_branch)
-  
-  finality_branch = sync_period_update['data'][0]['finality_branch']
-  parse_list(finality_branch) 
+  fork_version =  parse_hex_to_byte(sync_period_update['data'][0]['fork_version'])
 
-  fork_version =  sync_period_update['data'][0]['fork_version']
-  fork_version = parse_hex_to_byte(fork_version)
-
-  light_client_update = LightClientUpdate(
+  return LightClientUpdate(
     attested_header = attested_block_header,
     next_sync_committee = next_sync_committee,
     next_sync_committee_branch = next_sync_committee_branch,
@@ -109,36 +95,29 @@ def instantiate_sync_period_data(sync_period):
     # Contains the sync committee's bitfield and signature required for verifying the attested header.                                   @ben_eddington
     #
     sync_aggregate = sync_aggregate,
-    signature_slot =  attested_block_header.slot + 1                  # Slot at which the aggregate signature was created (untrusted)    
+    signature_slot =  attested_block_header.slot + 1     # Slot at which the aggregate signature was created (untrusted)    
   )
-  return light_client_update
-
 
 def instantiate_finality_update_data(update_message):
   attested_block_header = initialize_block_header(update_message['data']['attested_header']) 
   finalized_block_header = initialize_block_header(update_message['data']['finalized_header']) 
   sync_aggregate = initialize_sync_aggregate(update_message['data']['sync_aggregate'])
-  
-  finality_branch = update_message['data']['finality_branch']
-  parse_list(finality_branch) 
+  finality_branch = parse_list(update_message['data']['finality_branch'])
 
-  light_client_finality_update = LightClientFinalityUpdate (
+  return LightClientFinalityUpdate (
     attested_header = attested_block_header,
     finalized_header = finalized_block_header,
     finality_branch = finality_branch,
     sync_aggregate = sync_aggregate,
     signature_slot =  attested_block_header.slot + 1     
   )
-  return light_client_finality_update
-
 
 def instantiate_optimistic_update_data(update_message):
   attested_block_header = initialize_block_header(update_message['data']['attested_header']) 
   attested_sync_aggregate = initialize_sync_aggregate(update_message['data']['sync_aggregate'])
 
-  light_client_optimistic_update = LightClientOptimisticUpdate (
+  return LightClientOptimisticUpdate (
     attested_header = attested_block_header, 
     sync_aggregate = attested_sync_aggregate, 
     signature_slot = attested_block_header.slot + 1 
   )
-  return light_client_optimistic_update
