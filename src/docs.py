41843aaf162d303a8f57ac7dfa07b11c15e88f08
Bebbett <broycroft99@gmail.com>
2022-08-26 04:03:20 -1100

    Reorganize file structure

---
+++ src/docs.py
@@ -0,0 +1,98 @@
+"""                             
+      Get block header at slot N in period X = N // 8192
+      Ask node for current sync committee + proof of checkpoint root
+      Node responds with a snapshot
+      
+      Snapshot contains:
+      A. Header- Block's header corresponding to the checkpoint root
+      
+            The light client stores a header so it can ask for merkle branches to 
+            authenticate transactions and state against the header
+  
+      B. Current sync committee- Public Keys and the aggregated pub key of the current sync committee
+    
+            The purpose of the sync committee is to allow light clients to keep track
+            of the chain of beacon block headers. 
+            Sync committees are (i) updated infrequently, and (ii) saved directly in the beacon state, 
+            allowing light clients to verify the sync committee with a Merkle branch from a 
+            block header that they already know about, and use the public keys 
+            in the sync committee to directly authenticate signatures of more recent blocks.
+    
+      C. Current sync committee branch- Proof of the current sync committee in the form of a Merkle branch 
+  
+  
+"The light client stores the snapshot and fetches committee updates until it reaches the latest sync period."
+Get sycn period updates from current sync period to latest sync period
+
+A light client maintains its state in a store object of type LightClientStore and receives update objects of type LightClientUpdate. 
+Every update triggers process_light_client_update(store, update, current_slot) where current_slot is the current slot based on some local clock.
+
+
+===============================
+UNDERSTANDING THE BOOTSTRAP API
+===============================
+      Beacon Block header():          corresponding to Checkpoint root (or any block root specified)
+             slot:
+             proposer_index:
+             parent_root:
+             state_root:                       <------------------------- The god object that every validator must agree on
+             body_root:
+          
+      SyncCommittee():   
+             current_sync_committee:  pubkeys[]
+             aggregate_pubkey:
+  
+      *Additional information given*           <---------- Allows you to verify that the sync committee given is legitimate 
+             current_sync_committee_branch:
+  
+   Hashing the merkleized current_sync_committee root against the current_sync_committee_branch and
+   comparing this to the given state root verifies that the current sync committee is legitamite.  
+  
+  
+            The sync committee is so important because it allows the light client to keep up with the head of the blockchain
+            efficiently and in real time by only having to check the signature that goes with the head block.  If majority of 
+            committee signed the block, then you know that is the head of the chain.  Compare all information you want to check
+            against the beacon state for validity. 
+   
+  
+  ======================================
+  UNDERSTANDING THE COMMITTEE_UPDATE_API
+  ======================================
+
+  Committee update gives you...
+  
+      Attested Block header():         
+             slot:
+             proposer_index:
+             parent_root:
+             state_root:                       <------------------------- The god object that every validator must agree on
+             body_root:
+  
+      SyncCommittee():   
+             next_sync_committee: pubkeys[]
+             aggregate_pubkey:
+   
+      next_sync_committee_branch:
+  
+  
+  
+      Finalized Block header():                    
+             slot:
+             proposer_index:
+             parent_root:
+             state_root:
+             body_root:
+  
+      finality_branch:
+  
+      sync_aggregate:
+             sync_committee_bits:
+             sync_committee_signature:
+      fork_version:          
+  
+  
+  
+   
+  ... for each period you want:   from -> to 
+
+"""
